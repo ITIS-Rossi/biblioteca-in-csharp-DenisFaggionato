1678ef7d58f880fea2606477b1794e134b7bc67e
Denisfaggionato <denisfaggionato@gmail.com>
2023-05-07 21:39:50 +0200

    fase 4 non completa perchè non capisco na roba

---
+++ Biblioteca/Biblioteca.cs
@@ -0,0 +1,21 @@
+public class Biblioteca{
+
+    private string Nome;
+    private List<Libro> ElencoLibri;
+    private List<Autore> ElencoAutori;
+
+    public Biblioteca(string NomeBiblioteca){        
+        this.Nome = NomeBiblioteca;
+    }
+    public int NumeroLibri{
+        foreach(Autore liber in)
+    ;
+    }
+    public override string ToString(){
+        
+        return $"Biblioteca ";
+    }
+    
+
+
+}
